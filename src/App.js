f87c48a6e65209d65b9728d4363c14a80c8f9fde
Jeremie <jeremie@Jeremies-MacBook-Pro.local>
2022-04-28 22:01:29 -0700

    Initial Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-   
-      </header>
+    <div>
+      <Navbar />
+     
     </div>
   );
 }
