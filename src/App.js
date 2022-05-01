accb650788d29f876d6169e01b64b0b069c6c90b
Jeremie <jeremie@Jeremies-MacBook-Pro.local>
2022-04-30 18:02:26 -0700

    UI

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@
 import React from 'react';
 
 import Navbar from "./components/Navbar";
+import Carousel from "./components/Carousol";
+import Services from "./components/Services";
+import "./App.css"
+
 
 function App() {
   return (
-    <div>
+    <div className="main-container"> 
       <Navbar />
-     
+     <Carousel/>
+     <Services/>
     </div>
   );
 }
